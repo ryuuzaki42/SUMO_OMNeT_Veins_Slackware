81b644fc93d693e85082e4eca97c8c68023be654
João Batista <joao42lbatista@gmail.com>
2016-10-22 19:54:10 -0200

    Up and read_me

---
+++ 3_Veins/read_me.txt
@@ -0,0 +1,9 @@
+How to use each module is documented in the module itself in the form of doxygen comments.
+This can be transformed into API documentation by clicking Project > Generate NED 
+Documentation... in the IDE, or from the command line, by running make doxy.
+To start reading the module documenation, open doc/index.html in your browser.
+This gives you the most up-to-date form of documentation specific to your version of Veins.
+
+running make doxy for API documenation
+
+http://veins.car2x.org/documentation/modules/
---
+++ 3_Veins/update_veins.r
@@ -0,0 +1,13 @@
+## Error File: base/phyLayer/MappingBase.h, Line 739 Expression: dimSet.hasDimension(Dimension::time)
+
+    https://groups.google.com/forum/#!topic/omnetpp/OxRwN9w-VZM
+
+    http://stackoverflow.com/questions/35803072/debugging-runtime-differences-from-the-same-code-base/36055560#36055560
+
+    https://isocpp.org/wiki/faq/ctors#static-init-order
+
+    https://github.com/sommer/veins/commit/7807f47c#diff-f0987414551b7d10f8625f5d2853a14e
+
+    https://github.com/sommer/veins/releases
+
+## Dowload veins current ( > 4.3), now 4.4
