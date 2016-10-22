c8137df99233e0d212ede7439bbe68b967797d9d
João Batista <joao42lbatista@gmail.com>
2016-10-22 20:33:05 -0200

    Up readme SUMO 0.21.0

--- 1_SUMO/SUMO-0.21.0/readme.r
+++ 1_SUMO/SUMO-0.21.0/readme.r
@@ -1,4 +1,10 @@
-## libpng14.la not found
+# Install SUMO 0.21.0
+
+## install unixodbc unixodbc-2.3.4-x86_64-1gds.txz
+
+## Upgrade gdal to gdal-1.11.3-x86_64-1_SBo.tgz
+
+### libpng14.la not found
     # Add
     ln -s /usr/lib64/libpng16.la /usr/lib64/libpng14.la
 
@@ -10,6 +16,4 @@
 
     rm /usr/lib64/libpng14.la
 
-## Upgrade gdal to gdal-1.11.3-x86_64-1_SBo.tgz
-
-## install unixodbc unixodbc-2.3.4-x86_64-1gds.txz
+## To remove SUMO 0.21.0 and install SUMO 0.25.0 remove upgrade the gdal
