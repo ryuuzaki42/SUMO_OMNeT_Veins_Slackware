864453a5d91660f0e71c4362f344487be0d0b20c
João Batista <joao42lbatista@gmail.com>
2016-10-22 19:10:50 -0200

    SUMO

---
+++ 1_SUMO/SUMO-0.21.0/readme.r
@@ -0,0 +1,15 @@
+## libpng14.la not found
+    # Add
+    ln -s /usr/lib64/libpng16.la /usr/lib64/libpng14.la
+
+    ls -l /usr/lib64/libpng14.la
+        # /usr/lib64/libpng14.la -> /usr/lib64/libpng16.la
+
+## When remove the SUMO-021.0
+    ls -l /usr/lib64/libpng14.la
+
+    rm /usr/lib64/libpng14.la
+
+## Upgrade gdal to gdal-1.11.3-x86_64-1_SBo.tgz
+
+## install unixodbc unixodbc-2.3.4-x86_64-1gds.txz
---
+++ info.r
@@ -0,0 +1,47 @@
+## Versões dos simuladores
+SUMO = 0.25.0
+OMNeT = 4.6
+Veins = 4.4
+
+Setar no ~/.bashrc
+
+## veins .bashrc
+# Can be different in your PC
+export TCL_LIBRARY=/usr/lib64/tcl8.6
+export SUMO_HOME=/opt/sumo-0.25.0
+export PATH=$PATH:/opt/sumo-0.25.0/bin
+export PATH=$PATH:/opt/omnetpp-4.6/bin
+
+veinsFolder="/media/sda4/prog/veins_simulation/"
+export PATH=$PATH:$veinsFolder
+alias veins_folder_cd='cd $veinsFolder'
+
+# Not change
+veins_RunExperiment() {
+    dateStart=`date` # Start runing experiment
+    echo -e "\nExperiments staring at: $dateStart"
+
+    # get inital variables
+    iniFile=$1
+    runExperimentI=$2
+    runExperimentF=$3
+
+    # cd veins folder, make and go back
+    veins_folder_cd
+    echo -e "\nMake the files in the folder `pwd`\n"
+    make
+    cd -
+
+    if [ "$runExperimentI" == "" ]; then # test the input, if is "" will run -r 0, if is valid, run -r I..F
+        runExperimentI=0
+        runExperimentF=0
+    fi
+
+    echo -e "\nRunning: opp_run -r $runExperimentI..$runExperimentF -n ../../src/veins/ -u Cmdenv -l ../../out/gcc-debug/src/libveins_simulation.so "$iniFile"\n"
+    opp_run -r $runExperimentI..$runExperimentF -n ../../src/veins/ -u Cmdenv -l ../../out/gcc-debug/src/libveins_simulation.so "$iniFile"
+
+    echo -e "\n\nExperiment starts date: $dateStart"
+    echo -e "Experiment ends date: `date`\n" # End of runing experiment
+}
+alias opp-exc=veins_RunExperiment
+alias opp-exc-res="veins_RunExperiment > results/ExperimentOutput.r"
