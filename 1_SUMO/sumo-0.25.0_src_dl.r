2b467d82d61a300842b43224d1ae0ccc816dd963
João Batista <joao42lbatista@gmail.com>
2016-11-06 08:34:10 -0200

    SUMO(0.210 and 0.25.0) link download source code

---
+++ 1_SUMO/sumo-0.25.0_src_dl.r
@@ -0,0 +1,5 @@
+## Link to download the SUMO 0.21.0 source code:
+    ## https://sourceforge.net/projects/sumo/files/sumo/version%200.25.0/sumo-src-0.25.0.tar.gz
+
+## On GNU/Linux just run:
+wget -c https://sourceforge.net/projects/sumo/files/sumo/version%200.25.0/sumo-src-0.25.0.tar.gz
