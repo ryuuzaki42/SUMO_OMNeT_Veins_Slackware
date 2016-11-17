bf2a22b266f727c8435c2f35123ee448d7a4d993
João Batista <joao42lbatista@gmail.com>
2016-11-17 13:13:43 -0200

    Up *_install.r

--- 2_OMNET++/info_install_JB.r
+++ 2_OMNET++/info_install_JB.r
@@ -1,38 +1,30 @@
-##Instalar
+## Compile from source
     TCL_LIBRARY=/usr/lib64/tcl8.6/
     . setenv
     ./configure
     make
     make install-menu-item
 
-## trocar todos \n por nova linha (comando é as duas linhas
-    cat ~/.local/share/applications/opensim-ide.desktop | sed 's/\\n/\
-    /g' > ~/.local/share/applications/opensim-ide.desktop2
+## Change al "\n" for new line
+    sed -i 's/\\n/\n/g' ~/.local/share/applications/opensim-ide.desktop
 
-## remover o antigo
-    rm ~/.local/share/applications/opensim-ide.desktop
+## Run
+    omnetpp
 
-## renomear o atalho
-    mv ~/.local/share/applications/opensim-ide.desktop2 ~/.local/share/applications/opensim-ide.desktop
-
-## makepkg
-
-# ## troca \n por nova linha
-    # sed 's/\\n/\
-    # /g'
-
-## OMNeT++ estilo bonito # não utilize o tema oxygen default, use oxygen cold
-    Criar o arquivo em ~/.config/gtk-3.0/settings.ini
-    e no arquivo:
+## Good style for OMNeT # In kde change the theme oxygen default, to oxygen cold
+    Create a file: ~/.config/gtk-3.0/settings.ini
+    Inside the file put:
 
 [Settings]
 gtk-theme-name = oxygen-gtk
 gtk-fallback-icon-theme = gnome
 
-## Adicionar no ~/.bashrc
+## Add in ~/.bashrc
 #
 export TCL_LIBRARY=/usr/lib64/tcl8.6
+
 export SUMO_HOME=/opt/sumo-0.25.0
 export PATH=$PATH:/opt/sumo-0.25.0/bin
+
 export PATH=$PATH:/opt/omnetpp-4.6/bin
 #
